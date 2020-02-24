901ea3e8fdb302ba876f40c218b30d92cf471a37
Kris Hill <hillkri@oregonstate.edu>
2020-02-24 05:14:29 +0000

    added index/show routes/views, updated header/footer, added mongodb

--- app.js
+++ app.js
@@ -1,40 +1,76 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
 
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-	{name: "Salmon Creek", image: "https://live.staticflickr.com/3315/3647992218_7eda7a2a44_b.jpg"},
-	{name: "Lake Sabrina", image: "https://farm8.staticflickr.com/7460/13884819293_5f2656f5a2_b.jpg"},
-	{name: "Four Jeffries", image: "https://farm9.staticflickr.com/8583/16710089425_f257735c2d_b.jpg"},
-	{name: "Salmon Creek", image: "https://live.staticflickr.com/3315/3647992218_7eda7a2a44_b.jpg"},
-	{name: "Lake Sabrina", image: "https://farm8.staticflickr.com/7460/13884819293_5f2656f5a2_b.jpg"},
-	{name: "Four Jeffries", image: "https://farm9.staticflickr.com/8583/16710089425_f257735c2d_b.jpg"},
-	{name: "Salmon Creek", image: "https://live.staticflickr.com/3315/3647992218_7eda7a2a44_b.jpg"},
-	{name: "Lake Sabrina", image: "https://farm8.staticflickr.com/7460/13884819293_5f2656f5a2_b.jpg"},
-	{name: "Four Jeffries", image: "https://farm9.staticflickr.com/8583/16710089425_f257735c2d_b.jpg"}
-]
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+// 	name: "Lake Sabrina",
+// 	image: "https://farm8.staticflickr.com/7460/13884819293_5f2656f5a2_b.jpg",
+// 	description: "A beautiful lake high up in the eastern Sierra Nevadas."
+// 	}, 
+// 	function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else{
+// 			console.log("NEW CAMPGROUND:");
+// 			console.log(campground);
+// 		}
+// 	});
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds:campgrounds});
+	Campground.find({}, function(err, campgrounds){
+		if(err){
+			console.log(err);
+		} else{
+			res.render("index", {campgrounds:campgrounds});	
+		}
+	});
 });
 
 app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
-})
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
 
 app.get("/campgrounds/new", function(req, res){
-	res.render("new.ejs");
+	res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else{
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	
 });
 
 app.listen(3000, function(){

fe313e0535493b1ed49d4a98108dcf611b34dae5
Kris Hill <hillkri@oregonstate.edu>
2020-03-01 18:16:42 +0000

    added comments user authorization

--- routes/comments.js
+++ routes/comments.js
@@ -40,7 +40,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //COMMENTS EDIT ROUTE
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err){
 			res.redirect("back");
@@ -51,7 +51,7 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 //COMMENTS UPDATE ROUTE
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
 		if(err){
 			res.redirect("back");
@@ -62,7 +62,7 @@ router.put("/:comment_id", function(req, res){
 });
 
 //COMMENTS DESTROY ROUTE
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err){
 		if(err){
 			res.redirect("back");
@@ -78,6 +78,24 @@ function isLoggedIn(req, res, next){
 	   return next();
 	}
 	res.redirect("/login");
+};
+
+function checkCommentOwnership(req, res, next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err){
+				res.redirect("back");
+			}else{
+				if(foundComment.author.id.equals(req.user._id)){
+					next();
+				}else{
+					res.redirect("back");
+				}
+			}
+		});
+	}else{
+		res.redirect("back");
+	}
 };
 
 module.exports = router;
